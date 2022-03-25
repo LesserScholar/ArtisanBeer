867158ad94041a29ad5680a7458a7784aca6f129
Lesser Scholar <lesserscholar@gmail.com>
2022-03-25 17:00:04 +0200

    Add project files.

---
+++ SubModule.cs
@@ -0,0 +1,25 @@
+﻿using TaleWorlds.MountAndBlade;
+
+namespace ArtisanBeer
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+        
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+    }
+}
