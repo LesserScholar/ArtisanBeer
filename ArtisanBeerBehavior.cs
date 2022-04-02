13a365a5344b8e124e22a8f630f8b692e99da0b1
Lesser Scholar <lesserscholar@gmail.com>
2022-04-02 15:40:33 +0300

    Episode 8. Dialog system

--- ArtisanBeerBehavior.cs
+++ ArtisanBeerBehavior.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.AgentOrigins;
 using TaleWorlds.CampaignSystem.Encounters;
+using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.CampaignSystem.Settlements.Locations;
 using TaleWorlds.CampaignSystem.Settlements.Workshops;
 using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.ObjectSystem;
 
 namespace ArtisanBeer
 {
@@ -17,6 +20,51 @@ namespace ArtisanBeer
             CampaignEvents.OnWorkshopChangedEvent.AddNonSerializedListener(this, OnWorkshopChangedEvent);
             CampaignEvents.DailyTickTownEvent.AddNonSerializedListener(this, DailyTick);
             CampaignEvents.LocationCharactersAreReadyToSpawnEvent.AddNonSerializedListener(this, LocationCharactersAreReadyToSpawn);
+            CampaignEvents.OnSessionLaunchedEvent.AddNonSerializedListener(this, OnSessionLaunched);
+        }
+        ItemObject _artisanBeer;
+        private void OnSessionLaunched(CampaignGameStarter starter)
+        {
+            _artisanBeer = MBObjectManager.Instance.GetObject<ItemObject>("artisan_beer");
+            AddDialogs(starter);
+        }
+
+        private void AddDialogs(CampaignGameStarter starter)
+        {
+            {
+                starter.AddPlayerLine("tavernkeeper_talk_ask_artisan_beer", "tavernkeeper_talk", "tavernkeeper_artisan_beer", "Do you sell artisan beer?", null, null);
+                starter.AddDialogLine("tavernkeeper_talk_artisan_beer_a", "tavernkeeper_artisan_beer", "tavernkeeper_talk", "Bah. Greedy bastard at the brewery doesn't want to sell his stuff to me. Something about getting better rates selling directly to customers.", () =>
+                {
+                    foreach (var workshop in Settlement.CurrentSettlement.Town.Workshops)
+                    {
+                        if (workshop.WorkshopType.StringId == "brewery") return true;
+                    }
+                    return false;
+                }, null);
+                starter.AddDialogLine("tavernkeeper_talk_artisan_beer_b", "tavernkeeper_artisan_beer", "tavernkeeper_talk", "We don't have a brewery in town you'll have to look somewhere else.", null, null);
+            }
+            {
+                starter.AddDialogLine("artisan_brewer_talk", "start", "artisan_brewer", "Howdy. Would you like to purchace some Artisan Beer? One mug is 200 denars.",
+                    () => CharacterObject.OneToOneConversationCharacter == Settlement.CurrentSettlement.Culture.CaravanMaster, null);
+                starter.AddPlayerLine("artisan_brewer_buy", "artisan_brewer", "artisan_brewer_purchaced", "Sure, I'll take one.", null, () => {
+                    Hero.MainHero.ChangeHeroGold(-200);
+                    MobileParty.MainParty.ItemRoster.AddToCounts(_artisanBeer, 1);
+                }, 100, (out TextObject explanation) => {
+                    if (Hero.MainHero.Gold < 200)
+                    {
+                        explanation = new TextObject("Not enough money.");
+                        return false;
+                    } else
+                    {
+                        explanation = TextObject.Empty;
+                        return true;
+                    }
+                });
+                starter.AddDialogLine("artisan_brewer_thanks_for_business", "artisan_brewer_purchaced", "end", "Thank you come again!", null, null);
+
+                starter.AddPlayerLine("artisan_brewer_buy_refuse", "artisan_brewer", "artisan_brewer_declined", "Nah I'm good, thanks.", null, null);
+                starter.AddDialogLine("artisan_brewer_your_loss", "artisan_brewer_declined", "end", "Your loss.", null, null);
+            }
         }
 
         private void LocationCharactersAreReadyToSpawn(Dictionary<string, int> unusedUsablePointCount)
